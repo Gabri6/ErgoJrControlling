719da2e747d1e0744a4f0b6cad99be376e920fc7
Gabri6 <starling1220@gmail.com>
2023-03-10 13:15:57 +0100

    update gripperControl to implement movement depending on the torque sensed

--- main.cpp
+++ main.cpp
@@ -13,6 +13,10 @@ float _minJointCmd = 0;
 float _maxJointCmd = 1023;
 float _minJointAngle = -180.0f;
 float _maxJointAngle = 180.0f;
+int _targetJointTorqueGripper = 1140;
+float _proportionnalIncrement = 0.025f;
+float _proportionnalIncrementAbove = 0.01f;
+float _threshold= 1.9;
 
 int convertAnglesToJointCmd(float fJointAngle)
 { // y = ax + b
@@ -27,7 +31,7 @@ void goToHomePosition()
 	std::vector<uint16_t> l_vTargetJointPosition;
 	for (int l_joint = 0; l_joint < _nbJoints; l_joint++)
 		l_vTargetJointPosition.push_back(convertAnglesToJointCmd(0.0f));
-		_oDxlHandler.sendTargetJointPosition(l_vTargetJointPosition);
+	_oDxlHandler.sendTargetJointPosition(l_vTargetJointPosition);
 }
 
 void currentPos()
@@ -37,23 +41,77 @@ void currentPos()
 	_oDxlHandler.readCurrentJointPosition(l_vCurrentJointPosition);
 	
 	// display current joint position
-	std::cout << "vCurrentJointPosition= (";
-	for (int l_joint=0; l_joint < l_vCurrentJointPosition.size(); l_joint++)
-		std::cout << l_vCurrentJointPosition[l_joint] << ", ";
-	std::cout << ")" << std::endl;
+	//std::cout << "vCurrentJointPosition= (";
+	//for (int l_joint=0; l_joint < l_vCurrentJointPosition.size(); l_joint++)
+	//	std::cout << l_vCurrentJointPosition[l_joint] << ", ";
+	//std::cout << ")" << std::endl;
 }
 
-float gripperControl()
+int gripperControl()
 {
 	//read current joint 6 torque
 	std::vector<uint16_t> l_vCurrentJointTorque;
 	_oDxlHandler.readCurrentJointTorque(l_vCurrentJointTorque);
 	
+	//read current joint 6 position
+	std::vector<uint16_t> l_vCurrentJointPosition;
+	_oDxlHandler.readCurrentJointPosition(l_vCurrentJointPosition);
+	int joint6Position = l_vCurrentJointPosition[5];
+	
 	// display current joint 6 torque
-	std::cout << "vCurrentJoint6Torque= ";
+	std::cout << "CurrentJoint6Torque= ";
 	std::cout << l_vCurrentJointTorque[5] << std::endl;
-	float joint6Torque = l_vCurrentJointTorque[5];
-	return (float)joint6Torque;
+	int joint6Torque = l_vCurrentJointTorque[5];
+	
+	int jointTorqueDiff = joint6Torque - _targetJointTorqueGripper;
+	float angleIncrement = -jointTorqueDiff * _proportionnalIncrement;
+	std::cout<< "increment= " << angleIncrement<< std::endl;
+	float targetJoint6Position = joint6Position + angleIncrement;
+	std::cout << "joint torque diff= " <<abs(jointTorqueDiff) << std::endl;
+	
+	while (abs(angleIncrement) > _threshold)
+	{
+		
+		//read current joint 6 torque
+		std::vector<uint16_t> l_vCurrentJointTorque;
+		_oDxlHandler.readCurrentJointTorque(l_vCurrentJointTorque);
+		
+		//read current joint 6 position
+		std::vector<uint16_t> l_vCurrentJointPosition;
+		_oDxlHandler.readCurrentJointPosition(l_vCurrentJointPosition);
+		int joint6Position = l_vCurrentJointPosition[5];
+		
+		// display current joint 6 torque
+		std::cout << "vCurrentJoint6Torque= ";
+		std::cout << l_vCurrentJointTorque[5] << std::endl;
+		int joint6Torque = l_vCurrentJointTorque[5];
+		
+		int jointTorqueDiff = joint6Torque - _targetJointTorqueGripper;
+		if (jointTorqueDiff < 0)
+		{
+			float angleIncrement = -jointTorqueDiff * _proportionnalIncrement;
+			}
+		else{
+			float angleIncrement = 0;
+		}
+		std::cout<< "increment= " << angleIncrement<< std::endl;
+		float targetJoint6Position = joint6Position + angleIncrement;
+		std::cout << "joint torque diff= " <<jointTorqueDiff << std::endl;
+		
+		//set the position of each servomotor to 0
+		std::vector<uint16_t> l_vTargetJointPosition;
+		for (int l_joint = 0; l_joint < _nbJoints-1; l_joint++)
+			l_vTargetJointPosition.push_back(convertAnglesToJointCmd(0.0f));
+		//l_vTargetJointPosition.push_back(convertAnglesToJointCmd(90.0f));
+		//l_vTargetJointPosition.push_back(convertAnglesToJointCmd(90.0f));
+		//l_vTargetJointPosition.push_back(convertAnglesToJointCmd(-90.0f));
+		//l_vTargetJointPosition.push_back(convertAnglesToJointCmd(-90.0f));
+		//l_vTargetJointPosition.push_back(convertAnglesToJointCmd(45.0f));
+		l_vTargetJointPosition.push_back(targetJoint6Position);
+		_oDxlHandler.sendTargetJointPosition(l_vTargetJointPosition);
+	}
+		
+	return (int)joint6Torque;
 }
 
 
@@ -68,12 +126,14 @@ int main()
 	std::cout << std::endl;
 	
 	currentPos();
-	
+		
 	goToHomePosition();
-	
+		
 	currentPos();
 	
-	gripperControl();
+	int gripperTorque = gripperControl();
+	
+	goToHomePosition();
 	
 	// wait 1s
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
